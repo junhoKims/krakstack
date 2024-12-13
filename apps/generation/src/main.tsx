c132c7e2ff4fdc5aa8d5f5b4312bb433a93ce231
juno <kraksin12@gmail.com>
2024-12-13 17:48:48 +0900

    chore (generation): 테스트를 위한 vite 셋업

---
+++ apps/generation/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const element = document.getElementById('root')!;
+createRoot(element).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
