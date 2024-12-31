58606e484d2f5021d545c784bffe4fbe5a8e7cf9
krak <kraksin12@gmail.com>
2024-12-31 12:43:42 +0900

    feat (utils): `dayjs`를 사용한 date-utils 함수 구현
    dayjs와 동일하게 사용할 수 있도록 class를 만들어 모방

---
+++ packages/utils/src/common/date-utils/date-utils.test.ts
@@ -0,0 +1,110 @@
+import { dateUtils } from '@/common/date-utils/date-utils.js';
+
+const INVALIDE_DATE = null;
+
+describe('DateUtils', () => {
+  test('year()는 주어진 날짜의 연도를 반환', () => {
+    const year = dateUtils('2023-01-01').year();
+    expect(year).toBe(2023);
+  });
+  test('month()는 주어진 날짜의 월을 반환', () => {
+    const month1 = dateUtils('2023-01-01').month();
+    expect(month1).toBe(0); // 1월은 0으로 반환
+    const month2 = dateUtils('2023-02-01').month();
+    expect(month2).toBe(1); // 2월은 1으로 반환
+  });
+  test('date()는 주어진 날짜의 일을 반환', () => {
+    const date = dateUtils('2023-01-01').date();
+    expect(date).toBe(1);
+  });
+  test('day()는 주어진 날짜의 요일을 반환', () => {
+    const day1 = dateUtils('2023-01-01').day();
+    expect(day1).toBe(0); // 일요일은 0
+    const day2 = dateUtils('2023-01-02').day();
+    expect(day2).toBe(1); // 월요일은 1
+  });
+  test('hour()는 주어진 날짜의 시간을 반환', () => {
+    const hour = dateUtils('2023-01-01 13:00').hour();
+    expect(hour).toBe(13);
+  });
+  test('minute()는 주어진 날짜의 분을 반환', () => {
+    const minute = dateUtils('2023-01-01 13:15').minute();
+    expect(minute).toBe(15);
+  });
+  test('second()는 주어진 날짜의 초를 반환', () => {
+    const second = dateUtils('2023-01-01 13:15:25').second();
+    expect(second).toBe(25);
+  });
+  test('millisecond()는 주어진 날짜의 밀리초를 반환', () => {
+    const millisecond = dateUtils('2023-01-01 13:15:25.123').millisecond();
+    expect(millisecond).toBe(123);
+  });
+  test('set()는 주어진 날짜의 특정 단위를 설정한 값을 반환', () => {
+    const newDate = dateUtils('2023-01-01').set('year', 2020);
+    expect(newDate.year()).toBe(2020);
+  });
+  test('get()는 주어진 날짜의 특정 단위 값을 가져온다', () => {
+    const year = dateUtils('2023-01-01').get('year');
+    expect(year).toBe(2023);
+  });
+  test('add()는 주어진 날짜에 특정 단위만큼 시간을 더한 값을 반환', () => {
+    const newDate = dateUtils('2023-01-01').add(1, 'year');
+    expect(newDate.year()).toBe(2024);
+  });
+  test('subtract()는 주어진 날짜에서 특정 단위만큼 시간을 뺀 값을 반환', () => {
+    const newDate = dateUtils('2023-01-01').subtract(1, 'year');
+    expect(newDate.year()).toBe(2022);
+  });
+  test('startOf()는 주어진 날짜의 특정 단위의 시작 시간을 반환', () => {
+    const startOfMonth = dateUtils('2023-03-02').startOf('month');
+    expect(startOfMonth).toEqual(dateUtils('2023-03-01'));
+  });
+  test('endOf()는 주어진 날짜의 특정 단위의 끝 시간을 반환', () => {
+    const endOfMonth = dateUtils('2023-01-03').endOf('month');
+    expect(endOfMonth.month() + 1).toBe(1);
+    expect(endOfMonth.get('D')).toBe(31);
+  });
+  test('format()는 주어진 날짜를 특정 포맷으로 문자열로 반환', () => {
+    const formattedDate = dateUtils('2023-01-01').format('YYYY-MM-DD HH:mm:ss');
+    expect(formattedDate).toBe('2023-01-01 00:00:00');
+  });
+  test('diff()는 두 날짜의 차이를 특정 단위로 반환', () => {
+    const diff = dateUtils('2023-01-02').diff('2023-01-01', 'day');
+    expect(diff).toBe(1);
+  });
+  test('diff()는 두 날짜 중 하나라도 잘못된 날짜라면 NaN 반환', () => {
+    const diff1 = dateUtils(INVALIDE_DATE).diff(INVALIDE_DATE, 'day');
+    expect(diff1).toBe(NaN);
+    const diff2 = dateUtils(INVALIDE_DATE).diff(undefined, 'day');
+    expect(diff2).toBe(NaN);
+    const diff3 = dateUtils(undefined).diff(INVALIDE_DATE, 'day');
+    expect(diff3).toBe(NaN);
+    const diff4 = dateUtils('2023-01-01').diff(INVALIDE_DATE, 'day');
+    expect(diff4).toBe(NaN);
+  });
+  test('isValid()는 날짜가 올바른 날짜 포맷인지 여부를 반환', () => {
+    const validDate = dateUtils('2023-01-01').toDate();
+    const invalidDate = dateUtils('Fail').toDate();
+    expect(dateUtils(validDate).isValid()).toBe(true);
+    expect(dateUtils(invalidDate).isValid()).toBe(false);
+  });
+  test('잘못된 문자열 입력 시 Invalid Date 반환', () => {
+    const invalidDate = dateUtils('잘못된 날짜').toDate();
+    expect(dateUtils(invalidDate).isValid()).toBe(false);
+  });
+  test('undefined 입력 시 현재 날짜 반환', () => {
+    const dateFromUndefined = dateUtils(undefined).toDate();
+    expect(dateUtils(dateFromUndefined).isValid()).toBe(true);
+  });
+  test('null 입력 시 Invalid Date 반환', () => {
+    const dateFromNull = dateUtils(null).toDate();
+    expect(dateUtils(dateFromNull).isValid()).not.toBeUndefined();
+    expect(dateUtils(dateFromNull).isValid()).toBe(false);
+  });
+  test('현재 날짜가 오늘이면 true, 오늘이 아니면 false', () => {
+    const thisisToday = dateUtils().isToday();
+    expect(thisisToday).toBe(true);
+    const thisisNotToday = dateUtils().add(1, 'day').isToday();
+    expect(thisisNotToday).toBe(false);
+  });
+});
