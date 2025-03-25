c610f192e8c38d506cbc59b8e522157556cda783
krak <kraksin12@gmail.com>
2025-03-26 00:37:14 +0900

    feat (ui): Typo UI 스토리북 story 추가

--- packages/ui/src/core/typo/typo.tsx
+++ packages/ui/src/core/typo/typo.tsx
@@ -14,10 +14,12 @@ export interface TypoProps<T extends keyof TypoHTMLTag = 'p'>
   color?: string;
 }
 
+const DEFAULT_TAG = 'p';
+
 /**
- * Typo 텍스트 컴포넌트 UI
+ * @krakstack `Typo` UI
  */
-export const Typo = <T extends keyof TypoHTMLTag = 'p'>({
+export const Typo = <T extends keyof TypoHTMLTag = typeof DEFAULT_TAG>({
   as,
   variant = TypoVariant.BodySRegular,
   color,
@@ -30,7 +32,7 @@ export const Typo = <T extends keyof TypoHTMLTag = 'p'>({
     return (
       <>
         {createElement(
-          'p',
+          DEFAULT_TAG,
           { className: typoVariants({ variant, className }), style: { color, ...style }, ...rest },
           children
         )}
