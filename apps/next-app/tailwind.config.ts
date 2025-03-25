f5ffa71b0edf49f929baa5295c79a56b9c6c5795
krak <kraksin12@gmail.com>
2025-03-26 00:09:36 +0900

    fix (next-app): tailwindcss 범위를 잘못 지정한 문제 수정

--- apps/next-app/tailwind.config.ts
+++ apps/next-app/tailwind.config.ts
@@ -4,11 +4,7 @@ import { base, shadcnUi } from '@krakstack/tailwind-config/presets';
  * @type {import('tailwindcss').Config}
  */
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   presets: [base, shadcnUi],
   theme: {
     extend: {
